d059cb9c59c722bb47c2ed0a800c51febc28201d
Kolshgamer <90993492+Kolshgamer@users.noreply.github.com>
2025-04-13 11:39:10 -0500

    Add files via upload

---
+++ src/client/java/com/kolsh/minecraft/pregen/PreGenClient.java
@@ -0,0 +1,10 @@
+package com.kolsh.minecraft.pregen;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PreGenClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
